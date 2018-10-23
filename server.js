bf7675c24586aa98aca8dbb3a8709a1c36a6ac20
Ryan Williams <ryanjacobwilliams@gmail.com>
2018-10-23 10:16:10 -0500

    added static pages

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use('/', express.static('/public'));
+
+app.use('/events', express.static('./public/events-home'));
+
+app.use('/event/:id', express.static('./public/event-report'))
 
 app.listen(process.env.PORT || 8080);
 
