89bf4f0dea977e458670369324e5d04ae7453e7c
Ryan Williams <ryanjacobwilliams@gmail.com>
2018-10-23 11:14:19 -0500

    fixed initial test

--- server.js
+++ server.js
@@ -1,14 +1,62 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+
+//set up routers
+
+mongoose.Promise = global.Promise;
+
+const {DATABASE_URL, PORT} = require('./config');
 
 const app = express();
 
-app.use('/', express.static('/public'));
+app.use(morgan('common'));
 
-app.use('/events', express.static('./public/events-home'));
+app.use('/', express.static('public'));
 
-app.use('/event/:id', express.static('./public/event-report'))
+app.use('/events', express.static('public/events-home.html'));
 
-app.listen(process.env.PORT || 8080);
+app.use('*', (req, res) => {
+    return res.status(404).json({message: 'Not found'});
+})
+let server;
 
-module.exports = {app};
+function runServer(databaseUrl, port=PORT){
+    return new Promise ((resolve, reject)=>{
+        mongoose.connect(databaseUrl, err => {
+            if (err){
+                reject(err)
+            }
+            server = app.listen(port, ()=>{
+                console.log(`your app is listening on port ${port}`);
+                resolve();
+            }).on('error', err=>{
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+function closeServer(){
+    return mongoose.disconnect().then(()=>{
+        return new Promise ((resolve, reject)=>{
+            console.log(`closing server`);
+            server.close(err=>{
+                if (err){
+                    reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module){
+    runServer(DATABASE_URL).catch(err=> console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
--- test/test-root.js
+++ test/test-root.js
@@ -3,12 +3,21 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app} = require('../server');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe('root url', function(){
+describe('Static middleware connection', function(){
+    before(function(){
+        return runServer(TEST_DATABASE_URL);
+    });
+
+    after(function(){
+        return closeServer()
+    })
+    
     it('should respond with correct status', function(){
         return chai.request(app)
         .get('/')
